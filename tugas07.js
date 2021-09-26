865998f21d79b0ea0cc8011b3c8185dacd55c88d
whoisraa <raainvictus@gmail.com>
2021-09-26 19:10:27 +0700

    tugas 7 js

---
+++ tugas07.js
@@ -0,0 +1,11 @@
+var tinggiBadan = [150, 155, 160, 165, 170, 175, 180, 185, 190, 195];
+
+for (var i = 0; i < tinggiBadan.length; i++) {
+    console.log(tinggiBadan[i]);
+}
+
+console.log("");
+
+for (let x of tinggiBadan) {
+    console.log(x);
+}
